c79405bef75e57ef12a0611ba70fb11eb9a7f143
Arvin <45604781+NivraLeak@users.noreply.github.com>
2021-06-18 20:55:39 -0500

    Task 02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { OutcomeListComponent } from './components/outcome/outcome-list/outcome-list.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import {MatButtonModule} from "@angular/material/button";
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +15,9 @@ import { OutcomeListComponent } from './components/outcome/outcome-list/outcome-
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
